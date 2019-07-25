c96e7212a25736c4a85aff4dde818fd086573f66
eriksutiawan <eriksutiawan97@gmail.com>
2019-07-25 13:04:42 +0700

    Fixed for premission

--- resources/lang/en/global.php
+++ resources/lang/en/global.php
@@ -329,8 +329,6 @@ return [
             'created_at_helper' => '',
             'updated_at'        => 'Updated at',
             'updated_at_helper' => '',
-            'deleted_at'        => 'Deleted at',
-            'deleted_at_helper' => '',
         ],
     ],
     'product'                              => [
--- resources/views/admin/permissions/index.blade.php
+++ resources/views/admin/permissions/index.blade.php
@@ -1,14 +1,6 @@
 @extends('layouts.admin')
 @section('content')
-@can('permission_create')
-    <div style="margin-bottom: 10px;" class="row">
-        <div class="col-lg-12">
-            <a class="btn btn-success" href="{{ route("admin.permissions.create") }}">
-                {{ trans('global.add') }} {{ trans('global.permission.title_singular') }}
-            </a>
-        </div>
-    </div>
-@endcan
+
 <div class="card">
     <div class="card-header">
         {{ trans('global.permission.title_singular') }} {{ trans('global.list') }}
@@ -50,13 +42,6 @@
                                         {{ trans('global.edit') }}
                                     </a>
                                 @endcan
-                                @can('permission_delete')
-                                    <form action="{{ route('admin.permissions.destroy', $permission->id) }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
-                                        <input type="hidden" name="_method" value="DELETE">
-                                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                        <input type="submit" class="btn btn-xs btn-danger" value="{{ trans('global.delete') }}">
-                                    </form>
-                                @endcan
                             </td>
 
                         </tr>
@@ -69,40 +54,6 @@
 @section('scripts')
 @parent
 <script>
-    $(function () {
-  let deleteButtonTrans = '{{ trans('global.datatables.delete') }}'
-  let deleteButton = {
-    text: deleteButtonTrans,
-    url: "{{ route('admin.permissions.massDestroy') }}",
-    className: 'btn-danger',
-    action: function (e, dt, node, config) {
-      var ids = $.map(dt.rows({ selected: true }).nodes(), function (entry) {
-          return $(entry).data('entry-id')
-      });
-
-      if (ids.length === 0) {
-        alert('{{ trans('global.datatables.zero_selected') }}')
-
-        return
-      }
-
-      if (confirm('{{ trans('global.areYouSure') }}')) {
-        $.ajax({
-          headers: {'x-csrf-token': _token},
-          method: 'POST',
-          url: config.url,
-          data: { ids: ids, _method: 'DELETE' }})
-          .done(function () { location.reload() })
-      }
-    }
-  }
-  let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons)
-@can('permission_delete')
-  dtButtons.push(deleteButton)
-@endcan
-
-  $('.datatable:not(.ajaxTable)').DataTable({ buttons: dtButtons })
-})
 
 </script>
 @endsection
