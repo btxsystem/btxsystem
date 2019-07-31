d5469acad6803f96e5a6e9d33d0db5ee3f9ee402
putrajunarto <putrajunarto13@gmail.com>
2019-07-31 18:28:13 +0700

    update tree

--- resources/views/admin/tree/index.blade.php
+++ resources/views/admin/tree/index.blade.php
@@ -37,8 +37,8 @@
 				stroke-width: 2;
 			}*/
 			rect {
-				fill: white;
-				stroke: silver;
+				fill: #ebebeb;
+				stroke: #ebebeb;
 				width: 100px;
 				height: 100px;
 				stroke-width: 2;
@@ -75,74 +75,136 @@
       	var element = document.querySelector('#bah')
 				// And pass it to panzoom
 				panzoom(element);
-
-				$.ajax({
-				    type: 'GET', //THIS NEEDS TO BE GET
-				    url: '/admin/membership',
-				    success: function (data) {
-				        console.log(data);
-				    },
-				    error: function() { 
-				         console.log(data);
-				    }
-				});
 			});
-
 			var svg = d3.select("#example").append("svg")
-				.attr("width",1000).attr("height",600)
+				.attr("width",1000).attr("height",500)
 				.append("g").attr("transform", "translate(-300,0)")
 				.attr('id', 'bah');
-			var data = [
-				{"img":"https://img.icons8.com/bubbles/2x/user.png", "name":"Jarwo", "member":"id:1", "parent":"",},
-				{"img":"https://img.icons8.com/bubbles/2x/user.png", "name":"Sutri", "member":"id:2", "parent":"id:1"},
-				{"img":"https://img.icons8.com/bubbles/2x/user.png", "name":"Tejo", "member":"id:3", "parent":"id:1"},
-				{"img":"https://img.icons8.com/bubbles/2x/user.png", "name":"Sulis", "member":"id:4", "parent":"id:1"},
-			];
-			var dataStructure = d3.stratify()
-				.id(function(d){return d.member;})
-				.parentId(function(d){return d.parent;})
-				(data);
-			var treeStructure = d3.tree().size([1500,400]);
-			var information = treeStructure(dataStructure);
-			// console.log(information.descendants());
-			
-			var connections = svg.append("g").selectAll("path")
-				.data(information.links());
+			$.ajax({
+			    type: 'GET', //THIS NEEDS TO BE GET
+			    url: '/admin/membership',
+			    success: function (data) {
+			      var data = data[0];
+			      console.log(data)
+			   //    var data = {
+						//   "name": "A1",
+						//   "img":"https://img.icons8.com/bubbles/2x/user.png",
+						//   "level":"Silver",
+						// 	  "children": [
+						// 	    {
+						// 	      "name": "B1",
+						//   			"img":"https://img.icons8.com/bubbles/2x/user.png",
+						//   			"level":"Silver",
+						// 	      "children": [
+						// 	        {
+						// 	          "name": "C1",
+						//   					"img":"https://img.icons8.com/bubbles/2x/user.png",
+						//   					"level":"Silver",
+						// 	        },
+						// 	        {
+						// 	          "name": "C2",
+						//   					"img":"https://img.icons8.com/bubbles/2x/user.png",
+						//   					"level":"Silver",
+						// 	        },
+						// 	        {
+						// 	          "name": "C3",
+						// 	          "img":"https://img.icons8.com/bubbles/2x/user.png", 
+						// 	          "level":"Silver",
+						// 	        }
+						// 	      ]
+						// 	    },
+						// 	    {
+						// 	      "name": "B2",
+						// 	      "img":"https://img.icons8.com/bubbles/2x/user.png",
+						// 	      "level":"Silver",
+						// 	      "children": [
+						// 	        {
+						// 	          "name": "C4",
+						//   					"img":"https://img.icons8.com/bubbles/2x/user.png",
+						//   					"level":"Silver",
+						// 	        },
+						// 	        {
+						// 	          "name": "C5",
+						//   					"img":"https://img.icons8.com/bubbles/2x/user.png",
+						//   					"level":"Silver",
+						// 	        },
+						//         ]
+						// 	    },
+						// 	    {
+						// 	      "name": "B3",
+						// 	      "img":"https://img.icons8.com/bubbles/2x/user.png",
+						// 	      "level":"Silver",
+						// 	      "children": [
+						// 	        {
+						// 	          "name": "C6",
+						//   					"img":"https://img.icons8.com/bubbles/2x/user.png",
+						//   					"level":"Silver",
+						// 	        },
+						//         ]
+						// 	    }
+						// 	  ]
+						// }
+						// console.log(data)
+						// var dataStructure = d3.stratify()
+						// 	.id(function(d){return d.id;})
+						// 	.parentId(function(d){return d.sponsor_id;})
+						// 	(this.data);
+						var treeStructure = d3.tree().size([800,270]);
+						var root = d3.hierarchy(data);
+						treeStructure(root);
+						var information = treeStructure(root);
+						// console.log(information.descendants());
+						
+						var connections = svg.append("g").selectAll("path")
+							.data(information.links());
 
-			connections.enter().append("path")
-				.attr("d", function(d){
-					return "M" + d.source.x + "," + d.source.y + " v 100 H" +
-					d.target.x + " V" + d.target.y;
-				});
+						connections.enter().append("path")
+							.attr("d", function(d){
+								return "M" + d.source.x + "," + d.source.y + " v 100 H" +
+								d.target.x + " V" + d.target.y;
+							});
 
-			var rectangles = svg.append("g").selectAll("rect")
-				.data(information.descendants());
-			rectangles.enter().append("rect")
-				.attr("x", function(d){return d.x-50;})
-				.attr("y", function(d){return d.y-20;});
+						var rectangles = svg.append("g").selectAll("rect")
+							.data(information.descendants());
+						rectangles.enter().append("rect")
+							.attr("x", function(d){return d.x-50;})
+							.attr("y", function(d){return d.y-20;});
 
-			// var circles = svg.append("g").selectAll("circle")
-			// 	.data(information.descendants());
-			// circles.enter().append("circle")
-			// 	.attr("cx", function(d){return d.x})
-			// 	.attr("cy", function(d){return d.y})
-			// 	.attr("r", 5);
+						// var circles = svg.append("g").selectAll("circle")
+						// 	.data(information.descendants());
+						// circles.enter().append("circle")
+						// 	.attr("cx", function(d){return d.x})
+						// 	.attr("cy", function(d){return d.y})
+						// 	.attr("r", 5);
 
-			var names = svg.append("g").selectAll("text")
-				.data(information.descendants());
-			names.enter().append("text")
-				.text(function(d){return d.data.name;})
-				.attr("x", function(d){return d.x+0;})
-				.attr("y", function(d){return d.y+50;})
-				.classed("bigger", true);
+						var names = svg.append("g").selectAll("text")
+							.data(information.descendants());
+						names.enter().append("text")
+							.text(function(d){return "Name : " + d.data.name;})
+							.attr("x", function(d){return d.x+0;})
+							.attr("y", function(d){return d.y+50;})
+							.classed("bigger", true);
 
-			var image = svg.append("g").selectAll("image")
-				.data(information.descendants());
-			image.enter().append("image")
-				.attr("xlink:href", function(d){return d.data.img})
-				.attr("x", function(d){return d.x-30;})
-				.attr("y", function(d){return d.y-20;})
-				.classed("img-fluid", true);
+						var levels = svg.append("g").selectAll("text")
+							.data(information.descendants());
+						levels.enter().append("text")
+							.text(function(d){return "Level : " + d.data.level;})
+							.attr("x", function(d){return d.x+0;})
+							.attr("y", function(d){return d.y+70;})
+							.classed("bigger", true);
+
+						var image = svg.append("g").selectAll("image")
+							.data(information.descendants());
+						image.enter().append("image")
+							.attr("xlink:href", function(d){return d.data.img})
+							.attr("x", function(d){return d.x-30;})
+							.attr("y", function(d){return d.y-20;})
+							.classed("img-fluid", true);
+			    },
+			    error: function() { 
+			      console.log("Error");
+			    }
+			});
 
     </script>
 @stop
