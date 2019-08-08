1a9251205290444186decec7b110cb7947423e37
eriksutiawan <eriksutiawan97@gmail.com>
2019-08-08 19:08:29 +0700

    fix

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -52,7 +52,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapWebRoutes()
     {
-        Route::middleware('users')
+        Route::middleware('web')
              ->namespace($this->namespace)
              ->group(base_path('routes/web.php'));
     }
