14d8fa00d599a83582deb7203c4f2c741ffca209
eriksutiawan <eriksutiawan97@gmail.com>
2019-08-26 15:06:36 +0700

    fixed

--- resources/views/frontend/tree.blade.php
+++ resources/views/frontend/tree.blade.php
@@ -25,14 +25,16 @@
 							<input class="form-control" name="username" id="username" type="text" min="3" required>
 							<label class="form-label">Username</label>
 						</div>
-						<h5 style="color:red" id="username_danger"></h5>
+						<div>
+							<b style="color:red" id="username_danger"></b>
+						</div>
 					</div>
 					<div class="form-group form-float col-lg-12 col-md-12 col-sm-12 col-xs-12 row">
-						<div class="form-line form-group form-float col-lg-6 col-md-6 col-sm-6 col-xs-6">
+						<div class="form-line col-lg-6 col-md-6 col-sm-6 col-xs-6">
 							<input class="form-control" name="first_name" id="first_name" type="text" min="2" required>
 							<label class="form-label">First Name</label>
 						</div>
-						<div class="form-line form-group form-float col-lg-6 col-md-6 col-sm-6 col-xs-6">
+						<div class="form-line col-lg-6 col-md-6 col-sm-6 col-xs-6">
 							<input class="form-control" name="last_name" id="last_name" type="text" min="2" required>
 							<label class="form-label">Last Name</label>
 						</div>
