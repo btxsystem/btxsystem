92d25d1cabf393aca5254bbc9c4e5cf300376bcf
eriksutiawan <eriksutiawan97@gmail.com>
2019-08-27 16:30:06 +0700

    fixed

--- app/Http/Controllers/Member/EbookController.php
+++ app/Http/Controllers/Member/EbookController.php
@@ -23,12 +23,18 @@ class EbookController extends Controller
     }
 
     public function store(Request $request){
-        $data = [
-            'member_id' => Auth::user()->id,
-            'ebook_id' => $request->id,
-            'status' => 1,
-            'expired_at' => '2020-03-05'
-        ];
+        $trx = new TransactionMember;
+        $trx->member_id = Auth::user()->id ? Auth::user()->id : '-';
+        $trx->ebook_id = $request->id ? $request->id : '-' ;
+        $trx->status = 1;
+        $trx->expired_at = '2020-03-05';
+        //$data = [
+        //    'member_id' => Auth::user()->id,
+        //    'ebook_id' => $request->id,
+        //    'status' => 1,
+        //    'expired_at' => '2020-03-05'
+        //];
+        dd($trx);
         TransactionMember::create($data);
         return redirect()->route('member.ebook.index');
     }
