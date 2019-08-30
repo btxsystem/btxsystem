c66c471002e263426c8659f2bb500a40f229c23f
eriksutiawan <eriksutiawan97@gmail.com>
2019-08-30 14:53:44 +0700

    transaction

--- app/Http/Controllers/Member/TransactionController.php
+++ app/Http/Controllers/Member/TransactionController.php
@@ -33,7 +33,7 @@ class TransactionController extends Controller
     public function prospectedMemberHistory(){
         $data = DB::table('non_members')->join('transaction_non_members','non_members.id','=','transaction_non_members.non_member_id')
                                        ->join('ebooks','transaction_non_members.ebook_id','=','ebooks.id')
-                                       ->where('transaction_non_members.non_member_id','=',Auth::id())
+                                       ->where('transaction_non_members.member_id','=',Auth::id())
                                        ->select('ebooks.title','ebooks.price','ebooks.price_markup','transaction_non_members.created_at as date','non_members.username')->paginate(3);
         return response()->json(['transaction'=>$data]);
     }
