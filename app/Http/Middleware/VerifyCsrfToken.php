de69779f641bb789cc898c5505ae05b6cb5ee18a
asepmedia <edukasimedia25@gmail.com>
2019-09-02 13:05:24 +0700

    Update

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -19,6 +19,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        'backend-response-pay'
+        'backend-response-pay',
+        'response-pay'
     ]; 
 }
