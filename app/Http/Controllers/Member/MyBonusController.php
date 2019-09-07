9de8f37f2e9227d7693d550f9e38bdc67a0a2d49
eriksutiawan <eriksutiawan97@gmail.com>
2019-09-07 13:35:27 +0700

    fixed

--- app/Http/Controllers/Member/MyBonusController.php
+++ app/Http/Controllers/Member/MyBonusController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Member;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Auth; 
 use DB;
 use App\Models\TransactionMember;
 use App\Models\Ebook;
