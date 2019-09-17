50fdfd4ddbedb7c2b54e6a071ab80b52821c79d4
eriksutiawan <eriksutiawan97@gmail.com>
2019-09-17 09:45:59 +0700

    fixed

--- database/migrations/2019_08_20_203720_create_trigger_bonus_rewards.php
+++ database/migrations/2019_08_20_203720_create_trigger_bonus_rewards.php
@@ -22,18 +22,25 @@ class CreateTriggerBonusRewards extends Migration
                 IF new.pv_left >= 1000000 and new.pv_midle >= 1000000 and new.pv_right >= 1000000 and @rank = 8 and MOD(@result, 8) = 7 THEN
                     INSERT INTO got_rewards (`member_id`, `reward_id`,  `created_at`, `updated_at`) VALUES (new.id_member, 8, now(), now());
                     UPDATE pv_rewards SET updated_at = now(), pv_rewards.pv_left = pv_rewards.pv_left - 1000000, pv_rewards.pv_midle = pv_rewards.pv_midle - 1000000 , pv_rewards.pv_right = pv_rewards.pv_right - 1000000  WHERE pv_rewards.id_member = new.id_member;
+                END IF;
                 IF new.pv_left >= 330000 and new.pv_midle >= 330000 and new.pv_right >= 330000 and @rank >= 7 and MOD(@result, 8) = 6  THEN
                     INSERT INTO got_rewards (`member_id`, `reward_id`,  `created_at`, `updated_at`) VALUES (new.id_member, 7, now(), now());
+                END IF;
                 IF new.pv_left >= 110000 and new.pv_midle >= 110000 and new.pv_right >= 110000 and @rank >= 6 and MOD(@result, 8) = 5 THEN
                     INSERT INTO got_rewards (`member_id`, `reward_id`,  `created_at`, `updated_at`) VALUES (new.id_member, 6, now(), now());
+                END IF;
                 IF new.pv_left >= 35000 and new.pv_midle >= 35000 and new.pv_right >= 35000 and @rank >= 5 and MOD(@result, 8) = 4 THEN
                     INSERT INTO got_rewards (`member_id`, `reward_id`,  `created_at`, `updated_at`) VALUES (new.id_member, 5, now(), now());
+                END IF;
                 IF new.pv_left >= 12000 and new.pv_midle >= 12000 and new.pv_right >= 12000 and @rank >= 4 and MOD(@result, 8) = 3 THEN
                     INSERT INTO got_rewards (`member_id`, `reward_id`,  `created_at`, `updated_at`) VALUES (new.id_member, 4, now(), now());
+                END IF;
                 IF new.pv_left >= 4000 and new.pv_midle >= 4000 and new.pv_right >= 4000 and @rank >= 3 and MOD(@result, 8) = 2 THEN
                     INSERT INTO got_rewards (`member_id`, `reward_id`,  `created_at`, `updated_at`) VALUES (new.id_member, 3, now(), now());
+                END IF;
                 IF new.pv_left >= 1200 and new.pv_midle >= 1200 and new.pv_right >= 1200 and @rank >= 2 and MOD(@result, 8) = 1 THEN
                     INSERT INTO got_rewards (`member_id`, `reward_id`,  `created_at`, `updated_at`) VALUES (new.id_member, 2, now(), now());
+                END IF;
                 IF new.pv_left >= 400 and new.pv_midle >= 400 and new.pv_right >= 400 and @rank >= 1 and MOD(@result, 8) = 0 THEN
                     INSERT INTO got_rewards (`member_id`, `reward_id`,  `created_at`, `updated_at`) VALUES (new.id_member, 1, now(), now());
                 END IF;
