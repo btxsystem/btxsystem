27be068613bf0e934b5dcf8b66f50ec3ff6dbf11
eriksutiawan <eriksutiawan97@gmail.com>
2019-09-26 01:25:54 +0700

    fixed

--- routes/admin.php
+++ routes/admin.php
@@ -272,7 +272,7 @@ Route::group(['middleware' => 'admin'], function () {
     });
 
     //generate
-    Route::group(['prefix'=>'import','as'=>'import.'], function(){
+    /*Route::group(['prefix'=>'import','as'=>'import.'], function(){
         Route::get('', ['as' => 'index', 'uses' => 'ImportExcelController@index']);
         Route::post('excel', ['as' => 'excel', 'uses' => 'ImportExcelController@import_excel']);
         Route::post('tree', ['as' => 'tree', 'uses' => 'ImportExcelController@import_tree']);
@@ -282,7 +282,7 @@ Route::group(['middleware' => 'admin'], function () {
         Route::post('curse', ['as' => 'curse', 'uses' => 'ImportExcelController@curse']);
         Route::post('old-bonus', ['as' => 'old-bonus', 'uses' => 'ImportExcelController@oldBonus']);
         Route::post('account-name', ['as' => 'account-name', 'uses' => 'ImportExcelController@account_name']);
-    });
+    });*/
     
     
     Route::get('generate-mail', ['as' => 'generate-mail', 'uses' => 'Member\SendEmailOldMember@sendMail']);
