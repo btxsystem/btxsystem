b056e4bcb69cd14ebbf029e9bb871c4eeaf6bb47
eriksutiawan <eriksutiawan97@gmail.com>
2019-09-30 12:19:15 +0700

    fixed

--- routes/web.php
+++ routes/web.php
@@ -159,7 +159,7 @@ Route::group(['prefix' => 'member', 'as'=> 'member.'], function () {
         Route::get('history-pv', ['as' => 'history-pv', 'uses' => 'Member\PvController@getHistoryPv']);
         Route::get('daily-bonus-sponsor', ['as' => 'daily-bonus-sponsor', 'uses' => 'Member\DashboardController@getBonusSponsorDaily']);
         Route::get('daily-pairing', ['as' => 'daily-pairing', 'uses' => 'Member\DashboardController@getBonusPairing']);
-       // Route::get('generate', ['as' => 'generate', 'uses' => 'Member\PvController@generate']);
+        //Route::get('generate', ['as' => 'generate', 'uses' => 'Member\PvController@generate']);
         Route::get('my-transaction', ['as' => 'my-transaction', 'uses' => 'Member\TransactionController@myTransaction']);
         Route::get('prospected-member-transaction', ['as' => 'prospected-member-transaction', 'uses' => 'Member\TransactionController@prospectedMemberHistory']);
         Route::get('reward', ['as' => 'reward', 'uses' => 'Member\ProfileMemberController@getRewards']);
