d196792e5731ce090ef90dc86e5df9c59cabd983
eriksutiawan <eriksutiawan97@gmail.com>
2019-10-01 13:36:27 +0700

    fixed

--- resources/views/frontend/bonus/scripts.blade.php
+++ resources/views/frontend/bonus/scripts.blade.php
@@ -5,9 +5,13 @@
                 url: '{{route("member.select.bonus")}}',
                 data: data,
                 success:function(data){
+                    console.log(data);
+                    $('.total-commission').text(addCommas(data.total.nominal == null ? 0 : data.total.nominal));
                     $('.bonus-sponsor').text(addCommas(data.sponsor.nominal == null ? 0 : data.sponsor.nominal));
                     $('.bonus-sales-profit').text(addCommas(data.profit.nominal == null ? 0 : data.profit.nominal));
                     $('.bonus-pairing').text(addCommas(data.pairing.nominal == null ? 0 : data.pairing.nominal));
+                    $('.bonus-rewards').text(addCommas(data.rewards.nominal == null ? 0 : data.rewards.nominal));
+                    $('.bonus-event').text(addCommas(data.event.nominal == null ? 0 : data.event.nominal));
                 }
             });
         });
