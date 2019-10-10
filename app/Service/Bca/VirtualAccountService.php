d2d4ed7fb2e4f79eccc7584329159325904e91e7
asepmedia <edukasimedia25@gmail.com>
2019-10-10 09:24:06 +0700

    Update

--- app/Service/Bca/VirtualAccountService.php
+++ app/Service/Bca/VirtualAccountService.php
@@ -5,6 +5,8 @@ namespace App\Service\Bca;
 use App\Entities\Bca\VaBillEntity;
 use App\Entities\Bca\LanguageEntity;
 use App\Entities\Bca\DetailBillEntity;
+use App\Repositories\TransactionBillDetailRepository;
+use App\Repositories\TransactionBillRepository;
 use App\Repositories\TransactionVirtualAccountRepository;
 use App\Service\TransactionProductService;
 use App\Types\BcaStatusType;
@@ -54,47 +56,99 @@ class VirtualAccountService
     $requestID = $request->input('RequestID');
     $channelType = $request->input('ChannelType');
 
+    $transactionBill = (new TransactionBillRepository())
+      ->findByCustomerNumber($customerNumber);
+
     $checkInquiryBills = (new VaBillEntity())
       ->setCompanyCode($companyCode ?? '11210')
-      ->setCustomerNumber($customerNumber ?? '008271822372')
-      ->setRequestID($requestID ?? '201507131507262221400000001975')
+      ->setCustomerNumber($customerNumber)
+      ->setRequestID($requestID)
       ->setChannelType($channelType ?? '6014')
       ->setInquiryStatus("00")
       ->setCustomerName('Customer BCA Virtual Account')
       ->setCurrencyCode('IDR')
       ->setTotalAmount("150000.00")
       ->setSubCompany('00001')
-      ->setDetailBiils(function() {
-        $detailBills = [
-          (new DetailBillEntity())
-          ->setBillDescription(
-            ((new LanguageEntity())
-              ->setIndonesian('Maintenance')
-              ->setEnglish('Maintenance'))
-          )
-          ->setBillAmount("150000.00")
-          ->setBillNumber("008271822372")
-          ->setBillSubCompany("00001")
-        ];
+      // ->setDetailBiils(function() {
+      //   $detailBills = [
+      //     (new DetailBillEntity())
+      //     ->setBillDescription(
+      //       ((new LanguageEntity())
+      //         ->setIndonesian('Maintenance')
+      //         ->setEnglish('Maintenance'))
+      //     )
+      //     ->setBillAmount("150000.00")
+      //     ->setBillNumber("008271822372")
+      //     ->setBillSubCompany("00001")
+      //   ];
   
-        return $detailBills;
-      })
+      //   return $detailBills;
+      // })
       ->setInquiryReason(
         ((new LanguageEntity())
           ->setIndonesian('Sukses')
           ->setEnglish('Success'))
-      )
-      ->setFreeTexts(function() {
-        $freeTexts = [
+      );    
+
+      //check transaction bills
+      if($transactionBill) {
+        $checkInquiryBills->setTotalAmount($transactionBill->total_amount)
+        ->setCurrencyCode($transactionBill->currency_code)
+        ->setTotalAmount($transactionBill->total_amount)
+        ->setPaidAmount($transactionBill->paid_amount)
+        ->setCustomerName($transactionBill->user_type == 'member' ? $transactionBill->member->username : $transactionBill->nonMember->username);
+      } else {
+        $checkInquiryBills->setInquiryStatus('01')
+        ->setInquiryReason(
           ((new LanguageEntity())
-          ->setIndonesian('Sukses')
-          ->setEnglish('Success'))
-        ];
-  
-        return $freeTexts;
-      });    
+            ->setIndonesian('Gagal')
+            ->setEnglish('Failed'))
+        );
+      }
+
+
+      $this->validationBillPresentment($checkInquiryBills, $request);
+      
     
     return $checkInquiryBills;
+  }
+
+  /**
+   * Check Inquiry function
+   *
+   * @param [type] $builder
+   * @param [type] $request
+   * @return void
+   */
+  public function validationBillPresentment($builder, $request){
+    if(
+      $request->input('CompanyCode') == '' || 
+      $request->input('CustomerNumber') == '' || 
+      $request->input('RequestID') == '' || 
+      $request->input('ChannelType') == '' ||
+      $request->input('TransactionDate') == '' || 
+      !$this->validateFormatDate($request->input('TransactionDate'))
+    ) {
+      $builder->setInquiryStatus('01')
+        ->setInquiryReason(
+          ((new LanguageEntity())
+            ->setIndonesian('Gagal')
+            ->setEnglish('Failed'))
+        );
+    }
+  }
+
+  /**
+   * Validate Format Date function
+   *
+   * @param [type] $date
+   * @param string $format
+   * @return void
+   */
+  public function validateFormatDate($date, $format = 'd/m/Y H:i:s')
+  {
+    $d = \DateTime::createFromFormat($format, $date);
+    return $d && $d->format($format) === $date;
   }
 
   /**
@@ -221,15 +275,15 @@ class VirtualAccountService
         break;
       case ProductType::EBOOK_NONMEMBER:
         return $this->transactionProductService
-          ->ebookMember($customerNumber);
+          ->ebookNonMember($customerNumber);
         break;
       case ProductType::TOPUP_BITREX_POINT:
         return $this->transactionProductService
-          ->ebookMember($customerNumber);
+          ->topUpBitrexPoint($customerNumber);
         break;
       case ProductType::REGISTER_MEMBER:
         return $this->transactionProductService
-          ->ebookMember($customerNumber);
+          ->registerMember($customerNumber);
         break;
     }
 
