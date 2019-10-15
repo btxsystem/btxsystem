71d69de1026f0eabde4778ecc50d7d4085ee75d6
Rizal Rezamaulana <rizalrezamaulana.rrm@gmail.com>
2019-10-15 17:08:07 +0700

    Transfer Domestic BCA

--- app/Http/Controllers/Admin/BCAController.php
+++ app/Http/Controllers/Admin/BCAController.php
@@ -12,13 +12,14 @@ class BCAController extends Controller
 {
     public function getBalance()
     {
+        // return base64_encode('c3fd6d93-6aec-4ef1-be33-5e964f8eda16:bb03f953-5cc6-4a8d-a4a8-4384b20852cf');
         $bca = new BCA;
-        return $bca->balanceInformation();
+        return $bca->getTimestamp();
     }
 
     public function fundTransfer()
     {
-        $date = "2019-10-14";
+        $date = "2019-10-15";
         $accountnumber = "0201245681";
         $amount = "100000.00";
         $remark1 = "Test Transfer";
@@ -26,6 +27,22 @@ class BCAController extends Controller
 
         $bca = new BCA;
         $response = $bca->transfer($date, $accountnumber, $amount, $remark1, $remark2);
+
+        return $response;
+    }
+
+    public function domesticTransfer()
+    {
+        $date = "2018-05-03";
+        $accountnumber = "0201245501";
+        $accountname = "Tester";
+        $bankcode = "BRONINJA";
+        $amount = "100000.00";
+        $remark1 = "Test Transfer";
+        $remark2 = "Testing transfer";
+
+        $bca = new BCA;
+        $response = $bca->domesticTransfer($date, $accountnumber, $accountname, $bankcode, $amount, $remark1, $remark2);
 
         return $response;
     }
