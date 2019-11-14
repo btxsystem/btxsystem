9f786adbedcfc2ba14a79d4576e22e3b11189a7c
eriksutiawan <eriksutiawan97@gmail.com>
2019-11-14 04:13:17 -0500

    out

--- resources/views/member-v2/components/list-ebook.blade.php
+++ resources/views/member-v2/components/list-ebook.blade.php
@@ -261,10 +261,10 @@
 								<input class="form-check-input" type="radio" name="payment_method" id="ipay" value="ipay">
 								<label class="form-check-label" for="inlineRadio1">VA & OVO</label>
 							</div>
-							<div class="form-check form-check-inline">
+							{{--<div class="form-check form-check-inline">
 								<input class="form-check-input" type="radio" name="payment_method" id="va-bca" value="va-bca">
 								<label class="form-check-label" for="inlineRadio1">BCA VA</label>
-							</div>
+							</div>--}}
 					  </div>
 					  <h4>Total yang dibayar : IDR </span><b><span id="total_price"></h4></b>
 		      </div>
