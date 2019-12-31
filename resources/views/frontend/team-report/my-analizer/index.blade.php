81077f80bc1de6e522577bb9a961d8a9724a50e1
eriksutiawan <eriksutiawan97@gmail.com>
2019-12-31 04:48:19 -0500

    fix

--- resources/views/frontend/team-report/my-analizer/index.blade.php
+++ resources/views/frontend/team-report/my-analizer/index.blade.php
@@ -12,7 +12,7 @@
             </div>
         </div>
     </div>
-   
+
     <!-- <div class="pull-right col-lg-3 col-md-3 col-sm-3 col-xs-3">
 		<div class="form">
             <select data-column="2" class="form-control filter-select">
@@ -27,7 +27,7 @@
     &nbsp;
     <div class="col-lg-3 col-lg-offset-1  col-md-3 col-md-offset-1  col-sm-3 col-sm-offset-1 col-xs-3 col-xs-offset-1 ">
 		<div class="form">
-            <select data-column="2" class="form-control filter-select">
+            <select data-column="3" class="form-control filter-select">
             <option value="">-- All Rank --</option>
             <option value="-">No Rank</option>
              @foreach($ranks as $rank)
@@ -55,7 +55,7 @@
                     </thead>
                     <tbody>
                     </tbody>
-                </table> 
+                </table>
             </div>
         </div>
     </div>
@@ -75,17 +75,17 @@
               processing: true,
               serverSide: true,
               ajax: {
-                url: "{{ route('member.team-report.my-analizer') }}", 
+                url: "{{ route('member.team-report.my-analizer') }}",
               },
-              
+
               columns: [
                   { data: 'DT_RowIndex', name: 'DT_RowIndex', orderable: false, searchable: false },
-                  { data: 'full_name', name: 'full_name'},                  
-                  { data: 'username', name: 'username'},                  
-                  { data: 'ranking', name: 'ranking', className: "text-center" },                  
-                  { data: 'pv_left', name: 'rankipv_leftng', className: "text-center"  },                  
-                  { data: 'pv_middle', name: 'pv_middle', className: "text-center"  },                  
-                  { data: 'pv_right', name: 'pv_right', className: "text-center"  },                  
+                  { data: 'full_name', name: 'full_name'},
+                  { data: 'username', name: 'username'},
+                  { data: 'ranking', name: 'ranking', className: "text-center" },
+                  { data: 'pv_left', name: 'rankipv_leftng', className: "text-center"  },
+                  { data: 'pv_middle', name: 'pv_middle', className: "text-center"  },
+                  { data: 'pv_right', name: 'pv_right', className: "text-center"  },
               ]
           });
           $('.filter-select').change(function() {
@@ -93,9 +93,9 @@
               .search($(this).val())
               .draw();
           });
-          
+
         });
-       
+
       </script>
-      
+
 @endsection
