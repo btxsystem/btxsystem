20be66376bd396db8ba1904134749e4f63eddcbc
eriksutiawan <eriksutiawan97@gmail.com>
2020-01-09 05:05:28 -0500

    fix

--- resources/views/frontend/auth/header.blade.php
+++ resources/views/frontend/auth/header.blade.php
@@ -28,10 +28,10 @@
 </head>
 
 <body>
+
 <script src="{{asset('assets2/js/sweet.js')}}"></script>
 @include('sweet::alert')
 @extends('frontend.auth.style.header')
-
 <div id="app">
 <div class="modal fade" id="join" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
@@ -273,9 +273,9 @@
                     <li><a id="product" href="#">Our Product</a></li>
                     <li><a id="event" href="/event">Event</a></li>
                     <!-- <li><a href="#"><button class="btn btn-effect btn-info btn-buy" style="background: #b92240; margin-top: -10px;">JOIN</button></a></li> -->
-                    <!-- <li><a data-toggle="modal" data-target="#join"><button class="btn btn-effect btn-info btn-buy" style="background: #b92240; margin-top: -10px;">JOIN</button></a></li> -->
-                    {{-- <li><a href="#"><button class="btn btn-effect btn-info btn-buy" style="background: #b92240; margin-top: -10px;">JOIN</button></a></li>--}}
-                   <li><a data-toggle="modal" data-target="#join"><button class="btn btn-effect btn-info btn-buy" style="background: #b92240; margin-top: -10px;">JOIN</button></a></li>
+                    <!-- <li><a data-toggle="modal" data-target="#join"><button class="btn btn-effect btn-info btn-buy" style="background: #b92240; margin-top: -10px;">JOIN</button></a></li>-->
+                     <li><a href="#"><button class="btn btn-effect btn-info btn-buy" style="background: #b92240; margin-top: -10px;">JOIN</button></a></li>
+                   {{--<li><a data-toggle="modal" data-target="#join"><button class="btn btn-effect btn-info btn-buy" style="background: #b92240; margin-top: -10px;">JOIN</button></a></li>--}}
                     <!-- <li><a href="#"><button class="btn btn-effect btn-info btn-buy" style="background: #b92240; margin-top: -10px;">JOIN</button></a></li> -->
 
                   </ul>
