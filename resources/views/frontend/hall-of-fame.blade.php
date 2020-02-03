696efbc50b851b122b0e96cef4211fd3b70f25cc
asepmedia <edukasimedia25@gmail.com>
2020-02-03 07:26:42 +0700

    Update

--- resources/views/frontend/hall-of-fame.blade.php
+++ resources/views/frontend/hall-of-fame.blade.php
@@ -6,7 +6,7 @@
 <head>
 <style>
     .bg-main {
-        height: 100%; background-image:  url({{asset('assets3/img/hero-fallback.jpg')}}); background-size: cover;background-repeat:no-repeat;
+        height: 100vh; background-image:  url({{asset('assets3/img/hero-fallback.jpg')}}); background-size: cover;background-repeat:no-repeat;
     }
 
     footer, header {
