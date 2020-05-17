0e1ef09e6c5ecd20a71f90f18e53d75a1586f66d
asepmedia <edukasimedia25@gmail.com>
2020-05-17 16:35:33 +0700

    Update Notification

--- app/Http/Controllers/Admin/NotificationController.php
+++ app/Http/Controllers/Admin/NotificationController.php
@@ -6,6 +6,7 @@ use Auth;
 use App\Service\NotificationService;
 use DataTables;
 use Illuminate\Support\Carbon;
+use App\Models\Notification;
 
 class NotificationController
 {
@@ -29,8 +30,8 @@ class NotificationController
                 ->editColumn('created_at', function($row) {
                     return Carbon::createFromFormat('Y-m-d H:i:s', $row->created_at)->format('d M Y - H:i');
                 })
-                ->editColumn('isRead', function($row) {
-                    return $row->isRead==1 ? '<label class="btn btn-success">Read</label>' : '<label class="btn btn-danger">Unread</label> <label onclick="readNotif('.$row->id.')" class="btn btn-primary">View</label>';
+                ->addColumn('isRead', function($row) {
+                    return $row->isRead==1 ? '<label class="btn btn-success">Read</label><label onclick="deleteNotif('.$row->id.')" class="btn btn-danger">Delete</label>' : '<label class="btn btn-danger">Unread</label> <label onclick="readNotification('.$row->id.')" class="btn btn-primary">View</label>';
                 })
                 ->rawColumns(['isRead'])
                 ->make(true);
@@ -39,6 +40,94 @@ class NotificationController
     public function read($id)
     {
         $this->service->readNotif($id);
+    }
+
+    public function delete($id)
+    {
+        try {
+            $delete = Notification::where('id', $id)->delete();
+
+            if(!$delete) {
+                return response()->json([
+                    'status' => false
+                ]);
+            }
+
+            return response()->json([
+                'status' => true
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => false
+            ]);
+        }
+    }
+
+    public function deleteAll()
+    {
+        try {
+            $delete = Notification::query()->delete();
+
+            if(!$delete) {
+                return response()->json([
+                    'status' => false
+                ]);
+            }
+
+            return response()->json([
+                'status' => true
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => false
+            ]);
+        }
+    }
+
+    public function readAll()
+    {
+        try {
+            $update = Notification::where('isRead', 0)->update([
+                'isRead' => 1
+            ]);
+
+            if(!$update) {
+                return response()->json([
+                    'status' => false
+                ]);
+            }
+
+            return response()->json([
+                'status' => true
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => false
+            ]);
+        }
+    }
+
+    public function unreadAll()
+    {
+        try {
+            $update = Notification::query()->update([
+                'isRead' => 0
+            ]);
+
+            if(!$update) {
+                return response()->json([
+                    'status' => false
+                ]);
+            }
+
+            return response()->json([
+                'status' => true
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => false
+            ]);
+        }
     }
 
     public function generate()
