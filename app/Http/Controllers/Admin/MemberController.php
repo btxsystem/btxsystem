b38ad6961583f6ca926ddd0063b4ad5b65fdd1e2
asepmedia <edukasimedia25@gmail.com>
2020-06-11 11:48:35 +0700

    update

--- app/Http/Controllers/Admin/MemberController.php
+++ app/Http/Controllers/Admin/MemberController.php
@@ -760,7 +760,6 @@ class MemberController extends Controller
 
 
         } catch (\Exception $e) {
-            return $e; 
             Alert::error('Gagal Menambahkan Masa Aktif Ebook', 'Gagal3');
             return redirect()->back();
         }
