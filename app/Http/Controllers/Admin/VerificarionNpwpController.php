0426501bef9e6f2a05467f1393145cb662162b7f
asepmedia <edukasimedia25@gmail.com>
2020-10-28 12:41:05 +0700

    add verification

--- app/Http/Controllers/Admin/VerificarionNpwpController.php
+++ app/Http/Controllers/Admin/VerificarionNpwpController.php
@@ -14,16 +14,23 @@ class VerificarionNpwpController extends Controller
     public function index(){
          if (request()->ajax()) {
            // $data = Employeer::where('verification', 0);
-            $data = DB::table('employeers')->where('verification', 0)->orderBy('id','DESC');
+            $data = DB::table('employeers')->orderBy('id','DESC');
             return Datatables::of($data)
                     ->addColumn('name', function($data) {
                         return $data->first_name.' '.$data->first_name;
                     })
+                    ->addColumn('status_verification', function($data) {
+                        return $data->verification == 0 ? '<span class="label label-danger">Unvefified</span>' : '<span class="label label-success">Vefified</span>';
+                    })
                     ->addColumn('action', function($row) {
-                        $action = \Auth::guard('admin')->user()->hasPermission('Verification_npwp.verification') ? "<a href='#detail' data-toggle='modal' onclick='verif(`$row->username`)' id='verif-button' class='btn btn-success fa fa-check'></a>" : "";
+                        if($row->verification == 0) {
+                            $action = \Auth::guard('admin')->user()->hasPermission('Verification_npwp.verification') ? "<a href='#detail' data-toggle='modal' onclick='verif(`$row->username`)' id='verif-button' class='btn btn-success fa fa-check'></a>" : "";
+                        } else {
+                            $action = \Auth::guard('admin')->user()->hasPermission('Verification_npwp.verification') ? "<a href='#detail-unvefification' data-toggle='modal' onclick='unverif(`$row->username`)' id='verif-button' class='btn btn-danger fa fa-close'></a>" : "";
+                        }
                         return $action;
                     })
-                    ->rawColumns(['action'])
+                    ->rawColumns(['action', 'status_verification'])
                     ->make(true);
          }
          return view('admin.verif-npwp.index');
@@ -32,6 +39,12 @@ class VerificarionNpwpController extends Controller
     public function store(Request $request){
         $data = Employeer::where('username',$request->username)->select('id')->first();
         $npwp['verification'] = 1;
+        Employeer::find($data->id)->update($npwp);
+    }
+
+    public function unverified(Request $request){
+        $data = Employeer::where('username',$request->username)->select('id')->first();
+        $npwp['verification'] = 0;
         Employeer::find($data->id)->update($npwp);
     }
 }
