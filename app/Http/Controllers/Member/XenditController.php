cc58093162e18ad14f6d5a82a04fe783e0791149
erik <erik@amarbank.co.id>
2021-04-14 18:19:50 +0700

    add debug try xendit

--- app/Http/Controllers/Member/XenditController.php
+++ app/Http/Controllers/Member/XenditController.php
@@ -34,9 +34,10 @@ class XenditController extends Controller
     }
 
     public function callback(Request $request) {
-        header("Content-Type:application/json", 'x-callback-token: 8682835e2a23bcbc0f9d4a05a2bbdeac75a0e428583bf5563d54b4f411c62ef5');
-        $data = json_decode(file_get_contents('php://input'), true);
-        dd($request);
+        // header("Content-Type:application/json", 'x-callback-token: 8682835e2a23bcbc0f9d4a05a2bbdeac75a0e428583bf5563d54b4f411c62ef5');
+        // dd($data);
+        // $data = json_decode(file_get_contents('php://input'), true);
+        dd(response()->json($request->all()));
         $findData = Exend::where('external_id', $data['external_id'])->first();
         $statusTrx = 6;
         if($data['status'] == 'PAID') {
