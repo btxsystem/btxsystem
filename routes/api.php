ae8b7a48761858fe130673921a0ca0231c85c7ed
erik <erik@amarbank.co.id>
2021-04-15 20:18:06 +0700

    fix

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,9 @@ Route::group(['namespace' => 'Api', 'prefix' => 'v1'], function() {
   Route::post('/login', 'AuthController@login');
 });
 
-//Route::post('/xendit-callback', ['as' => 'xendit-callback', 'uses' => 'Member\XenditController@callback']);
-
 Route::post('/oauth/token', 'Api\OAuthController@token');
 
 Route::middleware('bca')->post('/va/bills', 'Bca\VirtualAccountController@bills');
 Route::middleware('bca')->post('/va/payments', 'Bca\VirtualAccountController@payments');
+
+Route::post('/xendit-callback', 'Member\XenditController@callback');
