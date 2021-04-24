d0c8e890c130e7371f2491eeb02f3ee3e9e069fe
erik <erik@amarbank.co.id>
2021-04-24 16:01:30 +0700

    add xendit list

--- app/Http/Controllers/Member/GenerateExpMemberController.php
+++ app/Http/Controllers/Member/GenerateExpMemberController.php
@@ -11,7 +11,7 @@ class GenerateExpMemberController extends Controller
     {
         $employ = Employeer::with('transaction_member')->get();
         foreach ($employ as $employe) {
-            $lastTrx = $employe->transaction_member[count($employe->transaction_member)-1]->expired_at;
+            $lastTrx = $employe->transaction_member[count($employe->transaction_member)-1]->created_at;
             $expmember = date('Y-m-d H:i:s', strtotime($lastTrx . " +1 year"));
             $employe->expired_at = $expmember;
             $employe->save();
