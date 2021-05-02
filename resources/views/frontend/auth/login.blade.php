43dedb6a7f4a0e7b6e99a7e67871f91d65695529
erik <erik@amarbank.co.id>
2021-05-02 23:11:34 +0700

    new task

--- resources/views/frontend/auth/login.blade.php
+++ resources/views/frontend/auth/login.blade.php
@@ -7,8 +7,12 @@
       <span aria-hidden="true">&times;</span>
     </button>
   </div>
-  <div class="toast-body">
-    <a target="_blank" href="/announcment">Update for Maintenance</a>
+  <div class="toast-body" style="font-size: 12px;">
+    <p>Berdasarkan Peraturan Pemerintah (PP) Republik Indonesia Nomor 29 Tahun 2021 tentang penyelenggaraan bidang perdagangan.</p>
+
+    <p>Dengan ini PT. Bitrexgo Solusi Prima telah melakukan update terhadap SIUPL terbaru.</p>
+
+    <p>Diantanya adalah pembaharuan kode etik dan marketing plan yang dapat dilihat pada masing-masing dashboard member.</p>
   </div>
 </div>
 </div>
