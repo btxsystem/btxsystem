a6c9ab7a9b2789cc99098560e5f146655140f16e
erik <erik@amarbank.co.id>
2021-08-23 17:10:00 +0700

    frontend PR

--- database/migrations/2021_08_14_104945_create_procedure_pairings.php
+++ database/migrations/2021_08_14_104945_create_procedure_pairings.php
@@ -13,14 +13,41 @@ class CreateProcedurePairings extends Migration
      */
     public function up()
     {
+        DB::unprepared('DROP PROCEDURE IF EXISTS `add_pv_pairing`');
         DB::unprepared('
             CREATE PROCEDURE add_pv_pairing(idm INT, pv INT)
             BEGIN
-                DECLARE parent, cek int;
+                DECLARE parent, cek, cek_rank, max_pv, total_pv int;
                 SET max_sp_recursion_depth=10000;
                 set parent = (select parent_id from `employeers` where id = idm);
                 set cek = (select sum(id_member) from pairings where id_member = parent);
+                set cek_rank = (select count(ebook.pv) from ebook INNER JOIN transaction_member ON ebook.id = transaction_member.ebook_id where transaction_member.id_member = parent);
                 set time_zone = "+07:00";
+                set max_pv = 0;
+                set total_pv = 0;
+                IF (select position from `employeers` where id = idm) = 0 THEN
+                    set total_pv = (select sum(`left`) from history_pv_pairing where id_member = parent AND DATE(created_at) >= CURDATE());
+                ELSEIF (select position from `employeers` where id = idm) = 1 THEN
+                    set total_pv = (select sum(`midle`) from history_pv_pairing where id_member = parent AND DATE(created_at) >= CURDATE());
+                ELSEIF (select position from `employeers` where id = idm) = 2 THEN
+                    set total_pv = (select sum(`right`) from history_pv_pairing where id_member = parent AND DATE(created_at) >= CURDATE());
+                END IF;
+                IF cek_rank < 100 and total_pv < 50 THEN
+                    set max_pv = max_pv + 50;
+                    IF max_pv > 50 THEN
+                        set pv = 50;
+                    END IF
+                ELSEIF (cek_rank >= 100 and cek_rank < 200) and total_pv < 100 THEN
+                    set max_pv = max_pv + 100;
+                    IF max_pv > 100 THEN
+                        set pv = 100;
+                    END IF
+                ELSEIF cek_rank >= 200 and total_pv < 200 THEN
+                    set max_pv = max_pv + 200;
+                    IF max_pv > 200 THEN
+                        set pv = 200;
+                    END IF
+                END IF;
                 IF parent is not null THEN
                     IF cek is null THEN
                         IF (select position from `employeers` where id = idm) = 0 THEN
