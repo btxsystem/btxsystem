a241869a44e6f324a1f88fbf6411a9d26bece39a
erik <erik@amarbank.co.id>
2022-02-07 13:44:19 +0700

    excel

--- app/Http/Controllers/Member/ExportExcelController.php
+++ app/Http/Controllers/Member/ExportExcelController.php
@@ -3,12 +3,8 @@
 namespace App\Http\Controllers\Member;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Carbon\Carbon;
 use Excel;
 use App\Exports\ExportAllMember;
-use App\Employeer;
 
 class ExportExcelController extends Controller
 {
